ddebc601830f1e48e1d2c42afac9f3e8ecc12d82
hrb90 <harrisonrbrown90@gmail.com>
2017-04-09 18:55:43 -0400

    clickable circles, sort of

--- bundle.js
+++ bundle.js
@@ -1,41 +1,41 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-
+/******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			i: moduleId,
 /******/ 			l: false,
 /******/ 			exports: {}
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.l = true;
-
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
+/******/
 /******/ 	// identity function for calling harmony imports with the correct context
 /******/ 	__webpack_require__.i = function(value) { return value; };
-
+/******/
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
 /******/ 		if(!__webpack_require__.o(exports, name)) {
@@ -46,7 +46,7 @@
 /******/ 			});
 /******/ 		}
 /******/ 	};
-
+/******/
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
 /******/ 	__webpack_require__.n = function(module) {
 /******/ 		var getter = module && module.__esModule ?
@@ -55,13 +55,13 @@
 /******/ 		__webpack_require__.d(getter, 'a', getter);
 /******/ 		return getter;
 /******/ 	};
-
+/******/
 /******/ 	// Object.prototype.hasOwnProperty.call
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-
+/******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
@@ -73,17 +73,45 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", { value: true });
+var distance = function (v1, v2) {
+    var s = function (x) { return x * x; };
+    return Math.sqrt(s(v1.x - v2.x) + s(v1.y - v2.y));
+};
 var GraphDrawingWrapper = (function () {
     function GraphDrawingWrapper(canvasId) {
         this.canvasEl = document.getElementById(canvasId);
         this.drawCircle = this.drawCircle.bind(this);
-        this.canvasEl.addEventListener("click", this.drawCircle);
+        this.handleClick = this.handleClick.bind(this);
+        this.canvasEl.addEventListener("click", this.handleClick);
+        this.vertices = [];
     }
-    GraphDrawingWrapper.prototype.drawCircle = function (e) {
+    GraphDrawingWrapper.prototype.clickVertex = function (v) {
+        console.log(v);
+    };
+    GraphDrawingWrapper.prototype.drawCircle = function (v) {
         var context = this.canvasEl.getContext('2d');
         context.beginPath();
-        context.arc(e.x, e.y, 20, 0, 2 * Math.PI);
+        context.arc(v.x, v.y, 20, 0, 2 * Math.PI);
         context.stroke();
+        this.vertices.push(v);
+    };
+    GraphDrawingWrapper.prototype.handleClick = function (e) {
+        var newVertex = { x: e.x, y: e.y, colors: [] };
+        var clickedVertex;
+        var overlappingVertex;
+        this.vertices.forEach(function (v) {
+            var dist = distance(v, newVertex);
+            if (dist < 20)
+                clickedVertex = v;
+            if (dist < 40)
+                overlappingVertex = v;
+        });
+        if (clickedVertex) {
+            this.clickVertex(clickedVertex);
+        }
+        else if (!overlappingVertex) {
+            this.drawCircle(newVertex);
+        }
     };
     return GraphDrawingWrapper;
 }());
--- src/canvas_wrapper.ts
+++ src/canvas_wrapper.ts
@@ -1,16 +1,64 @@
+interface Face {
+  incidentEdge: HalfEdge;
+}
+
+interface HalfEdge {
+  origin: Vertex;
+  twin: HalfEdge | undefined;
+  next: HalfEdge | undefined;
+  prev: HalfEdge | undefined;
+  incidentFace: Face | undefined;
+}
+
+interface Vertex {
+  x: number,
+  y: number,
+  colors: Array<string>,
+  incidentEdge: HalfEdge | undefined;
+}
+
+const distance = (v1: Vertex, v2: Vertex) => {
+  const s = (x : number) => x * x;
+  return Math.sqrt(s(v1.x - v2.x) + s(v1.y - v2.y));
+};
+
 export class GraphDrawingWrapper {
   canvasEl: HTMLCanvasElement;
+  vertices: Array<Vertex>;
 
   constructor(canvasId : string) {
     this.canvasEl = (<HTMLCanvasElement>document.getElementById(canvasId));
     this.drawCircle = this.drawCircle.bind(this);
-    this.canvasEl.addEventListener("click", this.drawCircle);
+    this.handleClick = this.handleClick.bind(this);
+    this.canvasEl.addEventListener("click", this.handleClick);
+    this.vertices = [];
   }
 
-  drawCircle(e : MouseEvent) {
+  clickVertex(v: Vertex) {
+    console.log(v);
+  }
+
+  drawCircle(v: Vertex) {
     let context = this.canvasEl.getContext('2d');
     context.beginPath();
-    context.arc(e.x, e.y, 20, 0, 2 * Math.PI);
+    context.arc(v.x, v.y, 20, 0, 2 * Math.PI);
     context.stroke();
+    this.vertices.push(v);
+  }
+
+  handleClick(e : MouseEvent) {
+    let newVertex = <Vertex>{ x : e.x, y : e.y, colors: [] }
+    let clickedVertex: Vertex | undefined;
+    let overlappingVertex: Vertex | undefined;
+    this.vertices.forEach((v : Vertex) => {
+      let dist = distance(v, newVertex);
+      if (dist < 20) clickedVertex = v;
+      if (dist < 40) overlappingVertex = v;
+    });
+    if (clickedVertex) {
+      this.clickVertex(clickedVertex);
+    } else if (!overlappingVertex) {
+      this.drawCircle(newVertex);
+    }
   }
 }
