574e1539be92fc93b1c43bed74731911e70de0da
hrb90 <harrisonrbrown90@gmail.com>
2017-04-10 00:19:34 -0400

    Start implementing DCEL

--- src/planar_graph.ts
+++ src/planar_graph.ts
@@ -1,5 +1,6 @@
 interface Face {
-  incidentEdge: HalfEdge;
+  infinite: boolean;
+  incidentEdge?: HalfEdge;
 }
 
 interface HalfEdge {
@@ -19,6 +20,11 @@ export interface Vertex {
   incidentEdge?: HalfEdge;
 }
 
+// Do the line segments from v1-v2 and v3-v4 intersect?
+const intersect = (v1: Vertex, v2: Vertex, v3: Vertex, v4: Vertex) => {
+  return true;
+}
+
 export class PlanarGraph {
   vertices: Array<Vertex>;
   edges: Array<HalfEdge>;
@@ -27,7 +33,7 @@ export class PlanarGraph {
   constructor () {
     this.vertices = [];
     this.edges = [];
-    this.faces = [];
+    this.faces = [{infinite: true}];
   }
 
   addVertex (v: Vertex) {
@@ -36,5 +42,35 @@ export class PlanarGraph {
 
   addEdge(v1: Vertex, v2: Vertex) {
     return true;
+  }
+
+  checkEdge(v1: Vertex, v2: Vertex) {
+
+  }
+
+  getBoundaryEdges(f: Face) {
+    let boundaryEdges = <Array<HalfEdge>>[];
+    if (f.incidentEdge) {
+      let currentEdge = f.incidentEdge;
+      while (!boundaryEdges.indexOf(currentEdge)) {
+        boundaryEdges.push(currentEdge);
+        currentEdge = currentEdge.next;
+      }
+    }
+    return boundaryEdges;
+  }
+
+  getIncidentFaces(v: Vertex) {
+    let incidentFaces = <Array<Face>>[];
+    if (v.incidentEdge) {
+      let currentEdge = v.incidentEdge;
+      while (!incidentFaces.indexOf(currentEdge.incidentFace)) {
+        incidentFaces.push(currentEdge.incidentFace);
+        currentEdge = currentEdge.twin.next;
+      }
+    } else {
+      //...
+    }
+    return incidentFaces;
   }
 }
