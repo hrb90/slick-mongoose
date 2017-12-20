b05eb79f25e8a22e8811e7176600bb9130af43f3
hrb90 <harrisonrbrown90@gmail.com>
2017-12-20 14:07:03 -0500

    Replace button with Mousetrap key binding

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import { GraphDrawingWrapper } from "./src/canvas_wrapper";
 import { fiveColor } from "./src/thomassen";
 import { animate } from "./src/animation";
+import { bind } from "mousetrap";
 
 document.addEventListener("DOMContentLoaded", () => {
   let wrapper = new GraphDrawingWrapper("canvas", 10);
 
-  document.getElementById("animate-button").addEventListener("click", () => {
-    if (!wrapper.frozen) {      
+  bind("enter", () => {
+    if (!wrapper.frozen) {
       wrapper.unhighlightVertex();
       fiveColor(wrapper.graph);
       animate(wrapper);
